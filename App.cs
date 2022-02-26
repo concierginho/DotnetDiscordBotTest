a259219094907ecf2dfbb0b77fc7d90267d7dfc8
concierginho <konradlenartmain@gmail.com>
2022-02-26 13:16:15 +0100

    init

---
+++ App.cs
@@ -0,0 +1,30 @@
+using Discord.Commands;
+using Discord.WebSocket;
+
+using DotnetDiscordBotBase.Config;
+using DotnetDiscordBotBase.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DotnetDiscordBotTest
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<BotBaseConfig>();
+                    services.AddSingleton<DiscordSocketClient>();
+                    services.AddSingleton<CommandService>();
+
+                    services.AddHostedService<BotBaseService>();
+                });
+    }
+}
